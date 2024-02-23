f103aa0a1fb71a3c677d5a9fb0839d13003f2f33
Michael Kreil <github@michael-kreil.de>
2024-02-23 22:17:48 +0100

    test: fix server test

--- src/lib/server.test.ts
+++ src/lib/server.test.ts
@@ -2,12 +2,11 @@ import Request from 'supertest';
 import express from 'express';
 import { startServer } from './server';
 import { jest } from '@jest/globals';
-import {  readFileSync  } from 'fs';
+import { readFileSync } from 'fs';
 import type Test from 'supertest/lib/test';
 import { resolve } from 'path';
 import { MockedBucket } from './bucket/bucket.mock.test';
 import type { AbstractBucket } from './bucket';
-
 
 jest.spyOn(console, 'log').mockReturnValue();
 jest.mock('express', () => express); // Mock express
@@ -87,54 +86,63 @@ describe('Server Tests', () => {
 			const response = await server.get('/healthcheck');
 			expect(response.status).toBe(200);
 			expect(response.text).toBe('ok');
+			expect(response.type).toBe('text/plain');
 		});
 
 		it('serve static file', async () => {
-			const response = await server.get('/static/file');
+			const response = await server.get('/static/package.json');
 			expect(response.status).toBe(200);
-			expect(response.text).toBe('static file');
+			expect(response.body).toMatchObject({ name: '@versatiles/google-cloud' });
+			expect(response.type).toBe('application/json');
 		});
 
 		it('serve versatiles meta', async () => {
 			const response = await server.get('/geodata/test.versatiles?meta.json');
 			expect(response.status).toBe(200);
 			expect(response.text).toMatch(/^{"vector_layers"/);
+			expect(response.type).toBe('application/json');
 		});
 
 		it('serve versatiles style', async () => {
 			const response = await server.get('/geodata/test.versatiles?style.json');
 			expect(response.status).toBe(200);
 			expect(response.text).toMatch(/^{"version":8/);
+			expect(response.type).toBe('application/json');
 		});
 
 		it('serve versatiles preview', async () => {
 			const response = await server.get('/geodata/test.versatiles?preview');
 			expect(response.status).toBe(200);
 			expect(response.text).toMatch(/^<!DOCTYPE html>/);
+			expect(response.type).toBe('text/html');
 		});
 
 		it('serve versatiles tile', async () => {
 			const response = await server.get('/geodata/test.versatiles?tiles/14/3740/4505');
 			expect(response.status).toBe(200);
 			expect(response.text).toContain('water_lines');
+			expect(response.type).toBe('application/x-protobuf');
 		});
 
 		it('handle missing versatiles tile', async () => {
 			const response = await server.get('/geodata/test.versatiles?tiles/10/0/0');
 			expect(response.status).toBe(204);
 			expect(response.text).toBe('');
+			expect(response.type).toBe('text/plain');
 		});
 
 		it('handle missing static file', async () => {
 			const response = await server.get('/static/missing/file');
 			expect(response.status).toBe(404);
 			expect(response.text).toBe('file "static/missing/file" not found');
+			expect(response.type).toBe('text/plain');
 		});
 
 		it('handle wrong versatiles request', async () => {
 			const response = await server.get('/geodata/test.versatiles?everest');
 			expect(response.status).toBe(400);
 			expect(response.text).toBe('get parameter must be "meta.json", "style.json", or "tile/{z}/{x}/{y}"');
+			expect(response.type).toBe('text/plain');
 		});
 	});
 
@@ -153,30 +161,35 @@ describe('Server Tests', () => {
 			const response = await server.get('/README.md');
 			expect(response.status).toBe(200);
 			expect(response.text).toBe(readFileSync(resolve(basePath, 'README.md'), 'utf8'));
+			expect(response.type).toBe('text/markdown');
 		});
 
 		it('handle missing static file', async () => {
 			const response = await server.get('/static/file');
 			expect(response.status).toBe(404);
 			expect(response.text).toBe('file "static/file" not found');
+			expect(response.type).toBe('text/plain');
 		});
 
 		it('serve versatiles meta', async () => {
 			const response = await server.get('/testdata/island.versatiles?meta.json');
 			expect(response.status).toBe(200);
 			expect(response.text).toMatch(/^{"vector_layers"/);
+			expect(response.type).toBe('application/json');
 		});
 
 		it('serve versatiles style', async () => {
 			const response = await server.get('/testdata/island.versatiles?style.json');
 			expect(response.status).toBe(200);
 			expect(response.text).toMatch(/^{"version":8/);
+			expect(response.type).toBe('application/json');
 		});
 
 		it('serve versatiles preview', async () => {
 			const response = await server.get('/testdata/island.versatiles?preview');
 			expect(response.status).toBe(200);
 			expect(response.text).toMatch(/^<!DOCTYPE html>/);
+			expect(response.type).toBe('text/html');
 		});
 	});
 });
