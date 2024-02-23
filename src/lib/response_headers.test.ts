221ad607420c7d5144868c6b23a99d2f80eb0a60
Michael Kreil <github@michael-kreil.de>
2024-02-23 22:23:31 +0100

    test: response_headers

--- src/lib/response_headers.test.ts
+++ src/lib/response_headers.test.ts
@@ -1,15 +1,56 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { ResponseHeaders } from './response_headers';
 
-
 describe('ResponseHeaders', () => {
-	it('should set and get response headers correctly', () => {
-		const headers = new ResponseHeaders();
+	let headers: ResponseHeaders;
 
-		headers.set('test-header', 'test-value');
-		expect(headers.get('test-header')).toBe('test-value');
+	beforeEach(() => {
+		headers = new ResponseHeaders();
+	});
 
-		headers.remove('test-header');
-		expect(headers.get('test-header')).toBeUndefined();
+	test('should initialize with default cache-control header', () => {
+		expect(headers.get('cache-control')).toBe('max-age=86400');
+	});
+
+	test('should allow adding and retrieving custom headers', () => {
+		headers.set('x-custom-header', 'value');
+		expect(headers.get('x-custom-header')).toBe('value');
+	});
+
+	test('should override default headers if provided in constructor', () => {
+		const customHeaders = new ResponseHeaders({ 'cache-control': 'max-age=0' });
+		expect(customHeaders.get('cache-control')).toBe('max-age=0');
+	});
+
+	test('should remove headers', () => {
+		headers.set('x-remove-me', 'gone');
+		headers.remove('x-remove-me');
+		expect(headers.get('x-remove-me')).toBeUndefined();
+	});
+
+	test('should lock headers and throw error on modification attempts', () => {
+		headers.lock();
+		expect(() => headers.set('x-new-header', 'value')).toThrow();
+		expect(() => headers.remove('cache-control')).toThrow();
+	});
+
+	test('toString should return JSON string of headers', () => {
+		headers.set('x-another-header', 'value');
+		const expectedString = JSON.stringify({
+			'cache-control': 'max-age=86400',
+			'x-another-header': 'value',
+		});
+		expect(headers.toString()).toBe(expectedString);
+	});
+
+	test('getHeaders should return the headers object', () => {
+		const customHeaders = { 'x-my-header': 'myValue' };
+		headers.set('x-my-header', customHeaders['x-my-header']);
+		expect(headers.getHeaders()).toEqual(expect.objectContaining(customHeaders));
+	});
+
+	test('getContentEncoding should parse and return correct EncodingTools object', () => {
+		headers.set('content-encoding', 'gzip');
+		expect(headers.getContentEncoding().name).toEqual('gzip');
 	});
 });
-
